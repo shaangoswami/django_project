1c9a779a64f54404e4361e7ab2ed9b1cbd0ac218
Shaan Goswami <145965851+shaangoswami@users.noreply.github.com>
2026-03-30 12:01:54 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import request
 from django.http import HttpResponse
 from django.shortcuts import render
-# ----------------------------------codewithharry video 6,7------------------------------
+
 def homepage(request):
     # return HttpResponse("<h1>Hello home page</h1> <a href='''https://www.youtube.com/playlist?list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9'''>Django CodeWithHarry</a>" )
     # params={'name':'shaan', 'mood': 'django'}
