6428a25ec2b353f8d86e89be2cbfb42831233130
Shaan Goswami <145965851+shaangoswami@users.noreply.github.com>
2026-04-01 10:36:11 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.homepage, name='home'),
     path('home/', views.homepage, name='home'),
     path('analyze/', views.analyze, name='analyze'),
-   
-}
+]
